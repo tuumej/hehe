ffb45672881e4c9f2af416ac789312b3a22304d2
momos2-app <lsy1021302@naver.com>
2021-07-19 15:59:11 +0900

    Update SetIdActivity.java
    회원탈퇴 팝업창 생성

--- app/src/main/java/com/example/hehe/SettingPage/SetIdActivity.java
+++ app/src/main/java/com/example/hehe/SettingPage/SetIdActivity.java
@@ -1,15 +1,20 @@
 package com.example.hehe.SettingPage;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.example.hehe.MainActivity;
 import com.example.hehe.R;
 
-public class SetIdActivity extends AppCompatActivity {
+public class SetIdActivity extends AppCompatActivity implements View.OnClickListener{
 
     Button change_btn;
 
@@ -18,6 +23,7 @@ public class SetIdActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_id);
 
+        findViewById(R.id.button).setOnClickListener(this);
         change_btn = findViewById(R.id.change_id_btn);
         change_btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,4 +33,34 @@ public class SetIdActivity extends AppCompatActivity {
             }
         });
     }
+
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.button:
+                new AlertDialog.Builder(this)
+                        .setMessage("진짜 탈퇴하시겠습니까 ?")
+                        .setPositiveButton("아니요", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Log.i("Dialog","탈퇴안함");
+                                Toast.makeText(getApplicationContext(),"탈퇴취소",Toast.LENGTH_LONG).show();
+
+
+                            }
+                        })
+                        .setNeutralButton("탈퇴", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Intent intent1 = new Intent(getApplicationContext(), MainActivity.class);  //탈퇴시 현재는 MainActivity로 돌아가게 설정했으나 구현완료시 login화면으로 변경 필요
+                                startActivity(intent1);
+
+                            }
+                        })
+                        .setCancelable(false) //뒤로가기 버튼으로 팝업창 안닫힘
+                        .show();
+                break;
+        }
+    }
+
+
 }
