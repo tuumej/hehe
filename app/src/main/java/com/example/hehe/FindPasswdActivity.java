7337f2b43c38c1b8475a735bfaa799c8b6cfe84a
momos2-app <lsy1021302@naver.com>
2021-07-19 16:33:20 +0900

    Create FindPasswdActivity.java
    비밀번호 찾기 페이지 연동

---
+++ app/src/main/java/com/example/hehe/FindPasswdActivity.java
@@ -0,0 +1,20 @@
+package com.example.hehe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class FindPasswdActivity extends AppCompatActivity {
+
+    Button find_btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_passwd);
+
+
+
+    }
+}
