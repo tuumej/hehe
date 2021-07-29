42eed873e59cb39dd4ef64148a6757debaa31661
momos2-app <lsy1021302@naver.com>
2021-07-29 15:07:36 +0900

    Create SignUpActivity.java
    회원가입창 생성

---
+++ app/src/main/java/com/example/hehe/Signup/SignUpActivity.java
@@ -0,0 +1,30 @@
+package com.example.hehe.Signup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.hehe.R;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    Button login_btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        login_btn = findViewById(R.id.button15);
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),SignInActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
