54fbaca1717ca32c14cedac6b4535ff95531e74a
momos2-app <lsy1021302@naver.com>
2021-07-29 15:07:30 +0900

    Create SignInActivity.java
    로그인 창 생성

---
+++ app/src/main/java/com/example/hehe/Signup/SignInActivity.java
@@ -0,0 +1,45 @@
+package com.example.hehe.Signup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.hehe.FindPasswdActivity;
+import com.example.hehe.InitActivity;
+import com.example.hehe.R;
+
+public class SignInActivity extends AppCompatActivity {
+
+    Button sign_btn;
+    Button fi_btn;
+    Button inti_btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+
+        sign_btn = findViewById(R.id.button12);
+        sign_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        fi_btn = findViewById(R.id.button13);
+        fi_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), FindPasswdActivity.class);
+                startActivity(intent);
+            }
+        });
+        
+
+    }
+}
