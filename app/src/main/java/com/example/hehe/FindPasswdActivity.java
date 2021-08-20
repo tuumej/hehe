32c302eef48b3a8b775ae9edfb0760a4bccb6023
momos2-app <lsy1021302@naver.com>
2021-08-20 11:22:38 +0900

    Update FindPasswdActivity.java
    기존 선언되어 있던 불필요한 버튼 제거

--- app/src/main/java/com/example/hehe/FindPasswdActivity.java
+++ app/src/main/java/com/example/hehe/FindPasswdActivity.java
@@ -2,19 +2,33 @@ package com.example.hehe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextWatcher;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.hehe.Signup.SignInActivity;
 
 public class FindPasswdActivity extends AppCompatActivity {
 
-    Button find_btn;
+    Button new_pw_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_passwd);
 
-
+        new_pw_btn = findViewById(R.id.button5);
+        new_pw_btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(getApplicationContext(), SignInActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
+
 }
