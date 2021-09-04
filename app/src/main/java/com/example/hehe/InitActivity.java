6ab76f966af60265eea8ccc5c8bf68617b454f25
momos2-app <lsy1021302@naver.com>
2021-09-04 16:03:12 +0900

    Update InitActivity.java

--- app/src/main/java/com/example/hehe/InitActivity.java
+++ app/src/main/java/com/example/hehe/InitActivity.java
@@ -1,29 +1,77 @@
 package com.example.hehe;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.hehe.fragment.SetFragment;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class InitActivity extends AppCompatActivity implements View.OnClickListener{
 
     private Context mContext;
 
+    Button click_main_btn;
+    Button click_main_btn2;
+    Button click_main_btn3;
+    Button click_main_btn4;
+
+
     private FloatingActionButton fab_main, fab_sub1, fab_sub2;
     private Animation fab_open, fab_close;
     private boolean isFabOpen = false;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_init);
         mContext = getApplicationContext();
+
+
+        click_main_btn = findViewById(R.id.button22);
+        click_main_btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        click_main_btn2 = findViewById(R.id.button21);
+        click_main_btn2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        click_main_btn3 = findViewById(R.id.button24);
+        click_main_btn3.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        click_main_btn4 = findViewById(R.id.button23);
+        click_main_btn4.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
 
         fab_open = AnimationUtils.loadAnimation(mContext,R.anim.fab_open);
         fab_close = AnimationUtils.loadAnimation(mContext,R.anim.fab_close);
