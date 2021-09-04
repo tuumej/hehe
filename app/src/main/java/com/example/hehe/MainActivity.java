ac285e04d5d12199546bdb7e794c9ba33210f090
momos2-app <lsy1021302@naver.com>
2021-09-04 16:03:15 +0900

    Update MainActivity.java

--- app/src/main/java/com/example/hehe/MainActivity.java
+++ app/src/main/java/com/example/hehe/MainActivity.java
@@ -7,9 +7,12 @@ import androidx.fragment.app.FragmentStatePagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TableLayout;
 import android.widget.Toolbar;
 
@@ -21,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
 
 private ViewPager viewPager;
 private BottomNavigationView bottomNavigationView;
+
+
 
 
 
