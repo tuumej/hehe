4833cb6166594a6af09484dea784700c5814f09a
tuumej <46086015+tuumej@users.noreply.github.com>
2021-10-02 00:21:07 +0900

    Update MainFragment.java

--- app/src/main/java/com/example/hehe/fragment/MainFragment.java
+++ app/src/main/java/com/example/hehe/fragment/MainFragment.java
@@ -1,5 +1,6 @@
 package com.example.hehe.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,19 +8,69 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.example.hehe.PopActivity;
 import com.example.hehe.R;
+import com.example.hehe.Sum.SumActivity;
 
 
 public class MainFragment extends Fragment {
+    Button btn_sum;
 
+    public MainFragment(){
 
-
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View v = inflater.inflate(R.layout.fragment_main, container, false);
+
+        btn_sum = v.findViewById(R.id.button19);
+        btn_sum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), PopActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+
+
+        btn_sum = v.findViewById(R.id.button28);
+        btn_sum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), SumActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+        btn_sum = v.findViewById(R.id.button29);
+        btn_sum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), SumActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+        btn_sum = v.findViewById(R.id.button30);
+        btn_sum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), SumActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+        return v;
+
     }
 }
