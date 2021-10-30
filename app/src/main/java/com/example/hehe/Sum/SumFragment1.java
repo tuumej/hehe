08d32b22a65e6c1355a10f3fa627ad12db03e311
tuumej <46086015+tuumej@users.noreply.github.com>
2021-10-30 16:14:44 +0900

    Update SumFragment1.java

--- app/src/main/java/com/example/hehe/Sum/SumFragment1.java
+++ app/src/main/java/com/example/hehe/Sum/SumFragment1.java
@@ -1,5 +1,6 @@
 package com.example.hehe.Sum;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -11,15 +12,74 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.hehe.R;
+import com.github.mikephil.charting.animation.Easing;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
 
 
 public class SumFragment1 extends Fragment {
 
+    PieChart pieChart;
+
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_sum1, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
 
-        return view;
+        View v = inflater.inflate(R.layout.fragment_sum1, container, false);
+
+        pieChart = v.findViewById(R.id.piechart);
+
+        pieChart.setUsePercentValues(true);
+        pieChart.getDescription().setEnabled(false);
+        pieChart.setExtraOffsets(10,10,10,0);
+
+        pieChart.setDragDecelerationFrictionCoef(0.95f);
+
+        //가운데 원형 뚫는거
+        pieChart.setDrawHoleEnabled(true);
+        pieChart.setHoleColor(Color.WHITE);
+        pieChart.setEntryLabelTextSize(10);
+        pieChart.setTransparentCircleRadius(0f);
+        pieChart.getDescription().setEnabled(false);
+
+        //원형 차트 고정
+        pieChart.setRotationEnabled(false);
+
+        Legend l = pieChart.getLegend();
+        l.setVerticalAlignment(Legend.LegendVerticalAlignment.BOTTOM);
+        l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.CENTER);
+        l.setOrientation(Legend.LegendOrientation.HORIZONTAL);
+
+
+        ArrayList<PieEntry> yValues = new ArrayList<PieEntry>();
+
+        yValues.add(new PieEntry(30f,"교통비"));
+        yValues.add(new PieEntry(30f,"생활용품"));
+        yValues.add(new PieEntry(30f,"식비"));
+        yValues.add(new PieEntry(30f,"취미"));
+        yValues.add(new PieEntry(30f,"기타"));
+
+        pieChart.animateY(1000, Easing.EasingOption.EaseInOutCubic); //애니메이션
+
+        PieDataSet dataSet = new PieDataSet(yValues,"");
+        dataSet.setSliceSpace(3f);
+        dataSet.setSelectionShift(5f);
+        dataSet.setColors(ColorTemplate.JOYFUL_COLORS);
+
+        PieData data = new PieData((dataSet));
+        data.setValueTextSize(10f);
+        data.setValueTextColor(Color.YELLOW);
+
+        pieChart.setData(data);
+
+        return v;
     }
 }
