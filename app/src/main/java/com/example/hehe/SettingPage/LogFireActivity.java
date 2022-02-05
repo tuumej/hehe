766984ac3cda19d58358a48974804085ecd70db7
momos2-app <lsy1021302@naver.com>
2022-02-05 12:56:29 +0900

    Create LogFireActivity.java

---
+++ app/src/main/java/com/example/hehe/SettingPage/LogFireActivity.java
@@ -0,0 +1,18 @@
+package com.example.hehe.SettingPage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import com.example.hehe.R;
+
+public class LogFireActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_fire);
+    }
+}
