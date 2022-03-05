a0a6df4456778d3cea1b7f587d2d4c9b1d64aa15
momos2-app <lsy1021302@naver.com>
2022-03-06 00:56:17 +0900

    Update LoginRequest.java
    ip주소 변경

---
+++ app/src/main/java/com/example/hehe/VS/PopActivitySpend.java
@@ -0,0 +1,16 @@
+package com.example.hehe.VS;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.hehe.R;
+
+public class PopActivitySpend extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pop_spend);
+    }
+}
