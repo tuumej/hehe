d2154c8cf0e554922841dfc4d7355bc4ee5bf501
momos2-app <lsy1021302@naver.com>
2022-03-06 00:26:10 +0900

    Update LoginRequest.java
    로그인 url 및 변수 명 변경

--- app/src/main/java/com/example/hehe/Signup/LoginRequest.java
+++ app/src/main/java/com/example/hehe/Signup/LoginRequest.java
@@ -10,15 +10,15 @@ import java.util.Map;
 public class LoginRequest extends StringRequest {
 
     // 서버 URL 설정 ( JSP 파일 연동 )
-    final static private String URL = "http://18.117.8.18:8080/planco/login.jsp";
+    final static private String URL = "http://192.168.98.1:8080/planco/loginAction2.do";
     private Map<String, String> map;
 
     public LoginRequest(String email, String passwd, Response.Listener<String> listener) {
         super(Method.POST, URL, listener, null);
 
         map = new HashMap<>();
-        map.put("email", email);
-        map.put("passwd", passwd);
+        map.put("E_MAIL", email);
+        map.put("PASSWD", passwd);
     }
 
     protected Map<String, String> getParams() throws AuthFailureError {
