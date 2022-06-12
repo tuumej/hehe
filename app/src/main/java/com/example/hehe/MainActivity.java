11932b5e8cd1d8d834ffe359b2a95d0db4389e60
momos2-app <lsy1021302@naver.com>
2022-06-12 15:00:50 +0900

    오예~

--- app/src/main/java/com/example/hehe/MainActivity.java
+++ app/src/main/java/com/example/hehe/MainActivity.java
@@ -14,31 +14,41 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TableLayout;
+import android.widget.TextView;
 import android.widget.Toolbar;
 
+import com.example.hehe.Signup.UserCacheInfo;
 import com.example.hehe.fragment.ViewPagerAdapter;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.tabs.TabLayout;
 
+import org.w3c.dom.Text;
+
 public class MainActivity extends AppCompatActivity {
 
-private ViewPager viewPager;
-private BottomNavigationView bottomNavigationView;
-
-
-
-
+    private ViewPager viewPager;
+    private BottomNavigationView bottomNavigationView;
+    private TextView textView, textView1,textView2,textView3,textView4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent intent = getIntent();
+        System.out.println("%%%%%%%%%%%%%%%%%%% :" +intent.getStringExtra("E_MAIL"));
+        UserCacheInfo.E_MAIL = intent.getStringExtra("E_MAIL");
+        UserCacheInfo.NICK_NAME = intent.getStringExtra("NICK_NAME");
+        UserCacheInfo.USER_KEY = intent.getStringExtra("USER_KEY");
+        UserCacheInfo.TOTAL_CASH = intent.getStringExtra("TOTAL_CASH");
+        //UserCacheInfo.NICK_NAME = "AMDIN";
+       //UserCacheInfo.E_MAIL = "AMDIN";
 
         viewPager = findViewById(R.id.view_pager);
         bottomNavigationView = findViewById(R.id.bottom_navigation);
 
         ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager(), FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
         viewPager.setAdapter(adapter);
+
 
         viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
             @Override
@@ -50,6 +60,12 @@ private BottomNavigationView bottomNavigationView;
             public void onPageSelected(int position) {
                 switch (position){
                     case 0:
+                        textView = (TextView)findViewById(R.id.fragmane_NICKNAME);
+                        //System.out.println(UserCacheInfo.NICK_NAME);
+                        textView.setText(UserCacheInfo.NICK_NAME);
+                        textView2 = (TextView)findViewById(R.id.first_cash_total);
+                        textView2.setText(UserCacheInfo.TOTAL_CASH);
+                        viewPager.setCurrentItem(0);
                         bottomNavigationView.getMenu().findItem(R.id.menu_main).setChecked(true);
                        break;
 
@@ -58,6 +74,10 @@ private BottomNavigationView bottomNavigationView;
                         break;
 
                     case 2:
+                        textView = (TextView)findViewById(R.id.fragementset_NICKNAME);
+                        //System.out.println(UserCacheInfo.NICK_NAME);
+                        textView.setText(UserCacheInfo.NICK_NAME);
+                        viewPager.setCurrentItem(2);
                         bottomNavigationView.getMenu().findItem(R.id.menu_set).setChecked(true);
                         break;
                 }
@@ -76,6 +96,11 @@ private BottomNavigationView bottomNavigationView;
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()){
                     case R.id.menu_main:
+                        textView = (TextView)findViewById(R.id.fragmane_NICKNAME);
+                        //System.out.println(UserCacheInfo.NICK_NAME);
+                        textView.setText(UserCacheInfo.NICK_NAME);
+                        textView2 = (TextView)findViewById(R.id.first_cash_total);
+                        textView2.setText(UserCacheInfo.TOTAL_CASH);
                         viewPager.setCurrentItem(0);
                         break;
 
@@ -85,6 +110,9 @@ private BottomNavigationView bottomNavigationView;
                         break;
 
                     case R.id.menu_set:
+                        textView = (TextView)findViewById(R.id.fragementset_NICKNAME);
+                        //System.out.println(UserCacheInfo.NICK_NAME);
+                        textView.setText(UserCacheInfo.NICK_NAME);
                         viewPager.setCurrentItem(2);
                         break;
                 }
