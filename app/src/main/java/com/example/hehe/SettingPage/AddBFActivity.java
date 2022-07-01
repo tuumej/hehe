1563682ab535d5d20b5fc5a5b2e99bf84fbb5b7d
momos2-app <lsy1021302@naver.com>
2022-07-02 02:54:56 +0900

    Merge branch 'master' of https://github.com/tuumej/hehe

--- app/src/main/java/com/example/hehe/SettingPage/AddBFActivity.java
+++ app/src/main/java/com/example/hehe/SettingPage/AddBFActivity.java
@@ -1,16 +1,40 @@
 package com.example.hehe.SettingPage;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.example.hehe.Adapter;
 import com.example.hehe.R;
 
+import java.util.ArrayList;
+
 public class AddBFActivity extends AppCompatActivity {
+
+    ArrayList<String> list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_bf);
+
+        list = new ArrayList<>();
+        list.add("아이템 1");
+        list.add("아이템 2");
+        list.add("아이템 3");
+        list.add("아이템 4");
+        list.add("아이템 5");
+        list.add("아이템 6");
+
+        Adapter adapter = new Adapter(list);
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+
+
+
+
     }
 }
